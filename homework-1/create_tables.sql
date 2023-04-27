1538c63940a75f9fb302348fa159c0deb85ffb8a
Dmitry Zakhs <d.zakhs@gmail.com>
2023-04-28 00:56:04 +0300

    Homework 1 add main.py, create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name varchar(100) PRIMARY KEY,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id varchar(100) NOT NULL,
+    customer_id varchar(100) NOT NULL,
+	employee_id varchar(100) NOT NULL,
+	order_date varchar(100) NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='033942') as conn:
+    with conn.cursor() as cur:
+        with open('north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+            data = csv.reader(file)
+            for row in data:
+                cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', row)
+
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='033942') as conn:
+    with conn.cursor() as cur:
+        with open('north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+           data = csv.reader(file)
+           for row in data:
+              cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='033942') as conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r', encoding='utf-8') as file:
+            data = csv.reader(file)
+            for row in data:
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
